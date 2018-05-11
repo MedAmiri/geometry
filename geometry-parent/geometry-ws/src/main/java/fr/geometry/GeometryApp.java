1372a6b1770f282ea804c6edb8356d7a1e828db0
MedAmiri <amiri.mohammed41@gmail.com>
2018-05-11 21:13:30 +0200

    init project geometry

---
+++ geometry-parent/geometry-ws/src/main/java/fr/geometry/GeometryApp.java
@@ -0,0 +1,12 @@
+package fr.geometry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeometryApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeometryApp.class, args);
+	}
+}
